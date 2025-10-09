a9daaf82f3960085e0088daba2bfef976f3f5ec7
Zhiang QI <an9qi@yandex.com>
2025-10-09 16:08:46 +0800

    feat:add PassThrough filter

--- include/pcd2pgm/pcd2pgm.hpp
+++ include/pcd2pgm/pcd2pgm.hpp
@@ -2,7 +2,6 @@
 #include <pcl/impl/point_types.hpp>
 
 #include "rclcpp/rclcpp.hpp"
-#include <sensor_msgs/msg/point_cloud2.hpp>
 
 class PCD2PGM : public rclcpp::Node
 {
@@ -10,9 +9,11 @@ public:
     PCD2PGM(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
     void loadPCDFile(const std::string & filename);
     void publishPCD();
+    void passThroughFilter(double thre_low, double thre_high, bool flag_in);
 private:
     std::string pcd_path_{"/ros2_ws/src/COD_NAV_NEXT/map/map.pcd"};
     rclcpp::Node::SharedPtr node_{};
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr pcd_;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_after_pass_through_;
 };
--- src/pcd2pgm.cpp
+++ src/pcd2pgm.cpp
@@ -1,6 +1,9 @@
 #include "pcd2pgm/pcd2pgm.hpp"
 #include "pcl_conversions/pcl_conversions.h"
 #include <pcl/io/pcd_io.h>
+#include "pcl/common/transforms.h"
+#include "pcl/filters/passthrough.h"
+#include "pcl/filters/radius_outlier_removal.h"
 
 PCD2PGM::PCD2PGM(const rclcpp::NodeOptions& options) : Node("pcd2pgm_node")
 {
@@ -11,6 +14,22 @@ void PCD2PGM::loadPCDFile(const std::string& filepath)
 {
     pcl::io::loadPCDFile<pcl::PointXYZ>(filepath,*pcd_);
 
+}
+
+void PCD2PGM::passThroughFilter(double thre_low, double thre_high, bool flag_in)
+{
+    auto filtered_cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+    pcl::PassThrough<pcl::PointXYZ> passthrough;
+    passthrough.setInputCloud(pcd_);
+    passthrough.setFilterFieldName("z");
+    passthrough.setFilterLimits(thre_low, thre_high);
+    passthrough.setNegative(flag_in);
+    passthrough.filter(*filtered_cloud);
+
+    cloud_after_pass_through_ = filtered_cloud;
+    RCLCPP_INFO(
+      get_logger(), "After PassThrough filtering: %lu points",
+      cloud_after_pass_through_->points.size());
 }
 
 int main(int argc, char** argv)
