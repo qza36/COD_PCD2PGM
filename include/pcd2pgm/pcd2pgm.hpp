68d64c23907a18e63e0d9fe4bf5923eaeb6977f0
Zhiang QI <an9qi@yandex.com>
2025-10-10 18:40:15 +0800

    feat: add radius Outlier Filter

--- include/pcd2pgm/pcd2pgm.hpp
+++ include/pcd2pgm/pcd2pgm.hpp
@@ -10,13 +10,18 @@ public:
     void loadPCDFile(const std::string & filename);
     void publishPCD();
     void passThroughFilter(double thre_low, double thre_high, bool flag_in);
+    void radiusOutlierFilter(
+    const pcl::PointCloud<pcl::PointXYZ>::Ptr & input_cloud, double radius, int thre_count);
 private:
     float thre_low;
     float thre_high;
+    float radius;
+    float thre_count;
     bool is_negative;
     std::string pcd_path_{};
     rclcpp::Node::SharedPtr node_{};
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr pcd_;
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_after_pass_through_;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_after_radius_;
 };
--- src/pcd2pgm.cpp
+++ src/pcd2pgm.cpp
@@ -10,16 +10,21 @@ PCD2PGM::PCD2PGM(const rclcpp::NodeOptions& options) : Node("pcd2pgm_node")
     declare_parameter("thre_low","");
     declare_parameter("thre_high","");
     declare_parameter("is_negative","false");
+    declare_parameter("radius","");
+    declare_parameter("thre_count","");
     declare_parameter("pcd_path","/ros2_ws/src/COD_NAV_NEXT/map/map.pcd");
 
     get_parameter("pcd_path",pcd_path_);
     get_parameter("thre_low",thre_low);
     get_parameter("thre_high",thre_high);
+    get_parameter("radius",radius);
+    get_parameter("thre_count",thre_count);
     get_parameter("is_negative",is_negative);
 
     pcd_ = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
     loadPCDFile(pcd_path_);
     passThroughFilter(thre_low,thre_high,is_negative);
+    radiusOutlierFilter(cloud_after_pass_through_,radius,thre_count);
 }
 void PCD2PGM::loadPCDFile(const std::string& filepath)
 {
@@ -42,6 +47,21 @@ void PCD2PGM::passThroughFilter(double thre_low, double thre_high, bool flag_in)
       get_logger(), "After PassThrough filtering: %lu points",
       cloud_after_pass_through_->points.size());
 }
+
+void PCD2PGM::radiusOutlierFilter(const pcl::PointCloud<pcl::PointXYZ>::Ptr& input_cloud, double radius, int thre_count)
+{
+    auto filtered_cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+    pcl::RadiusOutlierRemoval<pcl::PointXYZ> radius_outlier;
+    radius_outlier.setInputCloud(input_cloud);
+    radius_outlier.setRadiusSearch(radius);
+    radius_outlier.setMinNeighborsInRadius(thre_count);
+    radius_outlier.filter(*filtered_cloud);
+
+    cloud_after_radius_ = filtered_cloud;
+    RCLCPP_INFO(
+      get_logger(), "After RadiusOutlier filtering: %lu points", cloud_after_radius_->points.size());
+}
+
 
 int main(int argc, char** argv)
 {
