9d5f913574594a6a8308a0a9dd9dec89f11ec211
ZhiangQi <an9qi@yandex.com>
2025-11-16 21:07:01 +0800

    fix:remove hardcoded PclLoderParam

--- launch/pcd2pgm.launch.py
+++ launch/pcd2pgm.launch.py
@@ -4,6 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.logging import launch_config
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
@@ -15,7 +16,7 @@ def generate_launch_description():
     # Declare launch configuration variables that can access the launch arguments values
     param_file = LaunchConfiguration('param_file')
     visualization_config_file = LaunchConfiguration('visualization_config')
-
+    PclLoaderParam = LaunchConfiguration('pclLoderParam')
     # Declare launch arguments
     declare_param_file_cmd = DeclareLaunchArgument(
         'param_file',
@@ -30,11 +31,22 @@ def generate_launch_description():
             grid_map_demos_dir, 'config', 'pcd_grid_rviz.yaml'),
         description='Full path to the Gridmap visualization config file to use')
 
+    declare_PclLoaderParam_cmd = DeclareLaunchArgument(
+        'pclLoderParam',
+        default_value=os.path.join(
+            grid_map_demos_dir,'config','PclLoaderParameters.yaml'
+        )
+    )
+
     pcd_to_gridmap_demo_node = Node(
         package='pcd2pgm',
         executable='pcd2pgm',
         name='pcd_to_gridmap',
-        parameters=[param_file],
+        parameters=[param_file,
+                    {
+                        "pcd2gridmapConfig" : PclLoaderParam
+                    }
+        ],
         output="screen",
         respawn=True
     )
@@ -53,6 +65,7 @@ def generate_launch_description():
     # Add launch arguments to the launch description
     ld.add_action(declare_param_file_cmd)
     ld.add_action(declare_visualization_config_file_cmd)
+    ld.add_action(declare_PclLoaderParam_cmd)
 
     # Add node actions to the launch description
     ld.add_action(pcd_to_gridmap_demo_node)
